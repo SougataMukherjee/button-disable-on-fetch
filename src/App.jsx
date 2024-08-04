95421598f2cbef2af8654af5ed6161f5443b23b2
Sougata <sougatamukherjee47@gmail.com>
2024-08-04 13:24:56 +0530

    js concept push

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App() {
+  const [disable, setDisable] = useState(false);
+  const [data, setData] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+
+  async function fetchData() {
+    try {
+      setDisable(true);
+      setIsLoading(true);
+      const response = await fetch("https://reqres.in/api/users?page=2");
+      if (!response.ok) {
+        throw new Error("Response not OK");
+      }
+      const resData = await response.json();
+      setIsLoading(false);
+      setData(resData);
+    } catch (e) {
+      console.error("Error:", e);
+    }
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+    <div>
+      {isLoading && <p>Fetching...</p>}
+      {!isLoading && (
+        <button onClick={fetchData} disabled={disable}>
+          Click
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+      )}
+      <p>{data ? JSON.stringify(data) : "No data fetched"}</p>
+    </div>
+  );
 }
-
-export default App
